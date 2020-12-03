bc5e03a15af62a861a7188bb998b96313db29fb6
Sebastian Rojas <seba.nrc@gmail.com>
2020-12-03 07:16:26 -0300

    fixed sessions#new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by_email(params[:email])
-    unless user && user.authenticate(params[:password])
-      flash.now[:alert] = "Email or password is invalid"
-      render "new"
+    unless user&.authenticate(params[:password])
+      flash.now[:alert] = 'Email or password is invalid'
+      render 'new'
+      return
     end
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Logged in!"
+    redirect_to root_url, notice: 'Logged in!'
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out!"
+    redirect_to root_url, notice: 'Logged out!'
   end
 end
